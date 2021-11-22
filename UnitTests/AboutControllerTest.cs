fc16f57912dd173c291828a6cdd87cc66a51ccd9
MJSasha <sasha.ushankov@gmail.com>
2021-11-22 14:47:20 +0300

    Add AboutController

---
+++ UnitTests/AboutControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using VemsMusic.Controllers;
+using VemsMusic.Interfaces;
+using VemsMusic.Models;
+using Xunit;
+
+
+namespace UnitTests
+{
+    public class AboutControllerTest
+    {
+        [Fact]
+        public void IndexTest()
+        {
+            AboutController controller = new AboutController();
+
+            ViewResult viewResult = controller.Index() as ViewResult;
+
+            Assert.IsType<ViewResult>(viewResult);
+        }
+    }
+}
---
+++ VemsMusic/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VemsMusic.Controllers
+{
+    public class AboutController : Controller
+    {
+        [Route("~/AboutUs")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
