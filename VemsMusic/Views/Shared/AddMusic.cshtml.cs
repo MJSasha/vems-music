10f08a8e7ba7a606c61792504d132e19101ec4d5
MJSasha <sasha.ushankov@gmail.com>
2021-11-27 00:30:19 +0300

    Added creation of models

---
+++ VemsMusic/Views/Shared/AddMusic.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VemsMusic.Views.Shared
+{
+    public class AddMusicModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
