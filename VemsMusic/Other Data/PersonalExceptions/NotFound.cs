c11c49dc0642dff1aa1783c0012ca920fe244c47
MJSasha <sasha.ushankov@gmail.com>
2022-01-13 18:13:51 +0300

    Added documentation to NotFound

--- VemsMusic/Other Data/PersonalExceptions/NotFound.cs
+++ VemsMusic/Other Data/PersonalExceptions/NotFound.cs
@@ -7,10 +7,17 @@ namespace VemsMusic.Other_Data.PersonalExceptions
     /// </summary>
     public class NotFound : Exception
     {
+        /// <summary>
+        /// Fires when the element is not found in the database.
+        /// </summary>
         public NotFound()
         {
 
         }
+        /// <summary>
+        /// Fires when the element is not found in the database.
+        /// </summary>
+        /// <param name="message">Specification for the item you are looking for.</param>
         public NotFound(string message) : base(message)
         {
 
